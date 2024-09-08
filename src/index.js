74f1be1f11c9d5b3edf0856376d99bd0012cd063
descusa01 <daryllescusa@gmail.com>
2024-09-08 19:28:07 +0800

    reverted back to normal

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { webpack_public_path } from 'react-dev-utils';
 
-webpack_public_path.set('/', __dirname);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Home/>
+ <Home/>
 );
+
+ 
