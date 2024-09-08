df0d3aead5f4fd8ef5187958c939b0bb4e4248c0
descusa01 <daryllescusa@gmail.com>
2024-09-08 18:45:52 +0800

    Update index.js
    Imported the webpack_public_path function from react-dev-utils and set the public URL to the root URL (/) using the set method.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { webpack_public_path } from 'react-dev-utils';
 
-process.env.PUBLIC_URL = '/'; 
+webpack_public_path.set('/', __dirname);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
