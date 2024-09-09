14a5dca11dd120e8eaf1503de061850c0c97a4a8
descusa01 <daryllescusa@gmail.com>
2024-09-09 08:09:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './components/Home.jsx';
+import Home from './src/components/Home.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
