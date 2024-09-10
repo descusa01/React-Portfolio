4ad74620f22068fc98752d6fe1fa4c84ddbb3f88
descusa01 <daryllescusa@gmail.com>
2024-09-10 10:23:58 +0800

    update Appjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from '/components/Home.jsx';
+import Home from './components/Home.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <Home/>
-);
+function App(){
+ return(
+<Home/>
+ );   
+}
 
  
+export default App; 
