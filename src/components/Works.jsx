5ee8795b79ced4a82fba5d99c83be47d5ae0cf93
descusa01 <daryllescusa@gmail.com>
2024-09-11 16:38:54 +0800

    added duda

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
 import CarouselImage1 from ".//images/tindnd.png";
-import CarouselImage2 from ".//images/simongame.png";
-import CarouselImage3 from ".//images/drumkit.png";
-import CarouselImage4 from ".//images/dicee.png";
+import CarouselImage2 from ".//images/dudasite.png";
+import CarouselImage3 from ".//images/simongame.png";
+import CarouselImage4 from ".//images/drumkit.png";
+import CarouselImage5 from ".//images/dicee.png";
+import duda from ".//images/duda.png";
+
 
 
 function Works (){
@@ -29,8 +32,21 @@ return (
 </Carousel.Item>
 
 <Carousel.Item>
-  <a href="https://simon-game-one-omega.vercel.app" target="_blank" rel="noopener noreferrer">
+  <a href="https://teacherdaryllescusa.responsivewebsitebuilder.io/preview/38f0c698?t=1725354532382" target="_blank" rel="noopener noreferrer">
     <img src={CarouselImage2} className="d-block w-100" alt="Project 2"/>
+  </a>
+  <Carousel.Caption className="translucent-caption">
+    {/* <h4><i className="fas fa-star" /> First slide label</h4> */}
+    <div className="icon-container">
+      <img src={duda}/>
+      <i className="fa-brands fa-css3-alt" />
+    </div>
+  </Carousel.Caption>
+</Carousel.Item>
+
+<Carousel.Item>
+  <a href="https://simon-game-one-omega.vercel.app" target="_blank" rel="noopener noreferrer">
+    <img src={CarouselImage3} className="d-block w-100" alt="Project 2"/>
   </a>
   <Carousel.Caption className="translucent-caption">
     {/* <h4><i className="fas fa-star" /> First slide label</h4> */}
@@ -45,7 +61,7 @@ return (
 
 <Carousel.Item>
   <a href="https://drum-kit-peach-eight.vercel.app" target="_blank" rel="noopener noreferrer">
-    <img src={CarouselImage3} className="d-block w-100" alt="Project 3"/>
+    <img src={CarouselImage4} className="d-block w-100" alt="Project 3"/>
   </a>
   <Carousel.Caption className="translucent-caption">
     {/* <h4><i className="fas fa-star" /> First slide label</h4> */}
@@ -60,7 +76,7 @@ return (
 
 <Carousel.Item>
   <a href="https://dicee-game-ten.vercel.app" target="_blank" rel="noopener noreferrer">
-    <img src={CarouselImage4} className="d-block w-100" alt="Project 4"/>
+    <img src={CarouselImage5} className="d-block w-100" alt="Project 4"/>
   </a>
   <Carousel.Caption className="translucent-caption">
     {/* <h4><i className="fas fa-star" /> First slide label</h4> */}
